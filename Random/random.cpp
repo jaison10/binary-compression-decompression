33c3c8b7d1c8a3548edaf662acda28678a64dc4b
jaisondza <jaisonj1010@gmail.com>
2019-02-15 13:32:29 +0530

    added code1

---
+++ Random/random.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <stdlib.h>
+#include <time.h>
+
+using namespace std;
+int main(void)
+{
+       
+     srand(time(NULL));  // to prevent sequence repetition between runs   
+
+  
+     for(int i = 1; i <=8; i++)     // looping to print 10 numbers
+     {
+           cout<< rand( ) % 2;   // formula for numbers 
+     }
+
+     return 0;
+}
+
