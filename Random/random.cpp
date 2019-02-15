1eeec226a4a5d45e54e497faa3057219f2bd1943
Jaison DSouza B <jaisonj1010@gmail.com>
2019-02-15 15:45:39 +0530

    Update random.cpp

--- Random/random.cpp
+++ Random/random.cpp
@@ -9,7 +9,7 @@ int main(void)
      srand(time(NULL));  // to prevent sequence repetition between runs   
 
   
-     for(int i = 1; i <=8; i++)     // looping to print 10 numbers
+     for(int i = 1; i <=8; i++)     // looping to print 8 numbers
      {
            cout<< rand( ) % 2;   // formula for numbers 
      }
